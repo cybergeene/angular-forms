b7c6bf13819f0a1940253f44d5b04bbb56f909e3
Evgeniy.Mednov <e.mednov@mail.ru>
2018-08-21 17:56:55 +0300

    add some code

--- src/app/hero-form/hero-form.component.ts
+++ src/app/hero-form/hero-form.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import {Hero} from '../hero';
 
 @Component({
   selector: 'app-hero-form',
   templateUrl: './hero-form.component.html',
   styleUrls: ['./hero-form.component.css']
 })
-export class HeroFormComponent implements OnInit {
+export class HeroFormComponent {
 
-  constructor() { }
+  powers = ['Really Smart', 'Super Flexible',
+            'Super Hot', 'Weather Changer'];
 
-  ngOnInit() {
-  }
+  model = new Hero(18, 'Dr IQ', this.powers[0], 'Chuck Oversteet');
 
+  submitted = false;
+
+  onSubmit() { this.submitted = true; }
+
+  // TODO: Remove this when we're done
+  get diagnostic() { return JSON.stringify(this.model); }
 }
